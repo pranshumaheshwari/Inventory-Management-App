a63e3f1fb1418812fb1ef8d8a8a1cf8371d2e86d
Pranshu Maheshwari <maheshwari.pranshu511@gmail.com>
2019-07-30 15:51:53 +0530

    Changed batch file

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
-cd C:\Users\Sankalp Enterprises\Desktop\Inventory-Management-App
+cd C:\Inventory-Management-App
 
 start npm start
-ping localhost -n 10 >NUL
-start chrome http://localhost:3000 -incognito
+ping localhost -n 20 >NUL
+start chrome http://localhost:3000 -incognito -start-fullscreen
