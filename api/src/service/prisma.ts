81cc144c9b14bb2039b3ab7e58ac9db1b5d0fee6
Pranshu Maheshwari <maheshwari.pranshu511@gmail.com>
2022-12-30 10:55:46 +0530

    Added prisma middleware

--- api/src/service/prisma.ts
+++ api/src/service/prisma.ts
@@ -2,31 +2,177 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-// When creating invoice with inwards, update the raw material stock as well
-// TODO - Update this
-// prisma.$use(async (params, next) => {
-//     const result = await next(params)
-//     if (
-//         params.model === 'Invoice' &&
-//         params.action === 'create' &&
-//         params.args.data &&
-//         params.args.data.inwards
-//     ) {
-//         const rawMaterials = params.args.create.inwards.createMany.data
-//         for (let rm of rawMaterials) {
-//             await prisma.rm.update({
-//                 where: {
-//                     id: rm.rmId,
-//                 },
-//                 data: {
-//                     poPendingStock: {
-//                         increment: rm.quantity,
-//                     },
-//                 },
-//             })
-//         }
-//     }
-//     return result
-// })
+interface InwardsPoPending {
+    id: number
+    rmId: string
+    quantity: number
+    invoiceId: string
+    supplierId: string
+    user: string
+    poId: string
+}
+
+interface RequisitionOutwards {
+    id: number
+    rmId: string
+    quantity: number
+    user: string
+    requisitionId: number
+}
+
+prisma.$use(async (params, next) => {
+    const result: any = await next(params)
+
+    if (
+        params.model === 'InwardsPoPending' &&
+        params.action === 'update' &&
+        params.args.data &&
+        params.args.data.status
+    ) {
+        if (params.args.data.status === 'Accepted') {
+            // Check if PO need to be closed
+            const acceptedPoQtys = await prisma.inwardsPoPending.groupBy({
+                by: ['rmId'],
+                where: {
+                    poId: (result as InwardsPoPending).poId,
+                    status: 'Accepted',
+                },
+                _sum: {
+                    quantity: true,
+                },
+            })
+            const poQtys = await prisma.poDetails.findMany({
+                where: {
+                    poId: (result as InwardsPoPending).poId,
+                },
+                select: {
+                    rmId: true,
+                    quantity: true,
+                },
+            })
+            if (poQtys.length === acceptedPoQtys.length) {
+                let shouldClose = true
+                for (const poQty of poQtys) {
+                    for (const acceptedPoQty of acceptedPoQtys) {
+                        if (poQty.rmId === acceptedPoQty.rmId) {
+                            if (
+                                acceptedPoQty._sum.quantity &&
+                                poQty.quantity > acceptedPoQty._sum.quantity
+                            ) {
+                                shouldClose = false
+                                break
+                            }
+                        }
+                    }
+                    if (!shouldClose) {
+                        break
+                    }
+                }
+                if (shouldClose) {
+                    await prisma.po.update({
+                        where: {
+                            id: (result as InwardsPoPending).poId,
+                        },
+                        data: {
+                            status: 'Closed',
+                        },
+                    })
+                }
+            }
+        }
+        // Close invoice if no more pending entries
+        const pending = await prisma.inwardsPoPending.count({
+            where: {
+                invoiceId: (result as InwardsPoPending).invoiceId,
+                status: 'PendingPoVerification',
+            },
+        })
+        if (pending === 0) {
+            await prisma.invoice.update({
+                where: {
+                    id_supplierId: {
+                        id: (result as InwardsPoPending).invoiceId,
+                        supplierId: (result as InwardsPoPending).supplierId,
+                    },
+                },
+                data: {
+                    status: 'Closed',
+                },
+            })
+        }
+    }
+
+    if (
+        params.model === 'RequisitionOutward' &&
+        params.action === 'create' &&
+        params.args.data
+    ) {
+        const totalQtys = await prisma.requisition
+            .findUnique({
+                where: {
+                    id: (result as RequisitionOutwards).requisitionId,
+                },
+                select: {
+                    fg: {
+                        select: {
+                            bom: {
+                                select: {
+                                    rmId: true,
+                                    quantity: true,
+                                },
+                            },
+                        },
+                    },
+                    quantity: true,
+                },
+            })
+            .then((data) =>
+                data?.fg.bom.map((d) => ({
+                    rmId: d.rmId,
+                    quantity: d.quantity * data.quantity,
+                }))
+            )
+        const issuedQtys = await prisma.requisitionOutward.groupBy({
+            where: {
+                requisitionId: (result as RequisitionOutwards).requisitionId,
+            },
+            by: ['rmId'],
+            _sum: {
+                quantity: true,
+            },
+        })
+        if (totalQtys && totalQtys.length === issuedQtys.length) {
+            let shouldClose = true
+            for (const totalQty of totalQtys) {
+                for (const issuedQty of issuedQtys) {
+                    if (totalQty.rmId === issuedQty.rmId) {
+                        if (
+                            issuedQty._sum.quantity &&
+                            totalQty.quantity > issuedQty._sum.quantity
+                        ) {
+                            shouldClose = false
+                            break
+                        }
+                    }
+                }
+                if (!shouldClose) {
+                    break
+                }
+            }
+            if (shouldClose) {
+                await prisma.requisition.update({
+                    where: {
+                        id: (result as RequisitionOutwards).requisitionId,
+                    },
+                    data: {
+                        status: 'Closed',
+                    },
+                })
+            }
+        }
+    }
+
+    return result
+})
 
 export default prisma
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,30 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import './index.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import 'ag-grid-enterprise'
+import 'ag-grid-community/styles/ag-grid.css' // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-alpine.css' // Optional theme CSS
+import 'simplebar/src/simplebar.css'
 
-import 'ag-grid-enterprise';
-import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
-import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>
-);
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
