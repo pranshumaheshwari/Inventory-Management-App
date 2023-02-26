2df830ebaa469481f0cfa8c7494cb9eff1471ce9
Pranshu Maheshwari <maheshwari.pranshu511@gmail.com>
2023-02-26 13:07:31 +0530

    Changed connection string

--- api/seed/migrate.ts
+++ api/seed/migrate.ts
@@ -3,86 +3,84 @@ import { Prisma, PrismaClient, UsersType } from '@prisma/client'
 import { FieldInfo } from 'mysql'
 import { MysqlError } from 'mysql'
 import dotenv from 'dotenv'
-import { exit } from 'process'
 import mysql from 'mysql'
 
 const prisma = new PrismaClient()
 dotenv.config()
 
-const connection_url = process.env.DATABASE_URL_OLD
+const connection = mysql.createConnection({
+    user: 'root',
+    password: '',
+    database: 'store',
+    socketPath: '',
+})
+connection.connect()
 
-if (connection_url) {
-    const connection = mysql.createConnection(connection_url)
-    connection.connect()
+// USERS
+connection.query(
+    'SELECT * FROM users',
+    (
+        error: MysqlError,
+        res: {
+            username: string
+            password: string
+            type: UsersType
+        }[],
+        fields: FieldInfo[]
+    ) => {
+        if (error) throw error
+        prisma.users.createMany({
+            data: res.map((r) => ({ ...r, name: r.username })),
+        })
+    }
+)
 
-    // USERS
-    connection.query(
-        'SELECT * FROM users',
-        (
-            error: MysqlError,
-            res: {
-                username: string
-                password: string
-                type: UsersType
-            }[],
-            fields: FieldInfo[]
-        ) => {
-            if (error) throw error
-            prisma.users.createMany({
-                data: res.map((r) => ({ ...r, name: r.username })),
-            })
-        }
-    )
+// CUSTOMER
+connection.query(
+    'SELECT * FROM customer',
+    (
+        error: MysqlError,
+        res: {
+            code: string
+            name: string
+            address1: string
+            address2: string
+            city: string
+            state: string
+            GST_no: string
+            PAN_no: string
+        }[],
+        fields: FieldInfo[]
+    ) => {
+        if (error) throw error
+        prisma.customer.createMany({
+            data: res.map((r) => ({ ...r, gst: r.GST_no, id: r.code })),
+        })
+    }
+)
 
-    // CUSTOMER
-    connection.query(
-        'SELECT * FROM customer',
-        (
-            error: MysqlError,
-            res: {
-                code: string
-                name: string
-                address1: string
-                address2: string
-                city: string
-                state: string
-                GST_no: string
-                PAN_no: string
-            }[],
-            fields: FieldInfo[]
-        ) => {
-            if (error) throw error
-            prisma.customer.createMany({
-                data: res.map((r) => ({ ...r, gst: r.GST_no, id: r.code })),
-            })
-        }
-    )
+// SUPPLIER
+connection.query(
+    'SELECT * FROM supplier',
+    (
+        error: MysqlError,
+        res: {
+            code: string
+            name: string
+            address1: string
+            address2: string
+            city: string
+            state: string
+            GST_no: string
+            PAN_no: string
+        }[],
+        fields: FieldInfo[]
+    ) => {
+        if (error) throw error
+        prisma.supplier.createMany({
+            data: res.map((r) => ({ ...r, gst: r.GST_no, id: r.code })),
+        })
+    }
+)
 
-    // SUPPLIER
-    connection.query(
-        'SELECT * FROM supplier',
-        (
-            error: MysqlError,
-            res: {
-                code: string
-                name: string
-                address1: string
-                address2: string
-                city: string
-                state: string
-                GST_no: string
-                PAN_no: string
-            }[],
-            fields: FieldInfo[]
-        ) => {
-            if (error) throw error
-            prisma.supplier.createMany({
-                data: res.map((r) => ({ ...r, gst: r.GST_no, id: r.code })),
-            })
-        }
-    )
-
-    connection.end()
-} else {
-    exit(1)
-}
+connection.end()
