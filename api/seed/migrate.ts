68ca748164e7ea1b3d4fb4586ed07eeace7eda73
Pranshu Maheshwari <maheshwari.pranshu511@gmail.com>
2023-03-22 13:07:10 +0530

    Added migration scripts

--- api/seed/migrate.ts
+++ api/seed/migrate.ts
@@ -1,206 +1,161 @@
-import {
-    FgCategory,
-    Prisma,
-    PrismaClient,
-    RmCategory,
-    UsersType,
-} from '@prisma/client'
-
-import { FieldInfo } from 'mysql'
-import { MysqlError } from 'mysql'
-import dotenv from 'dotenv'
-import mysql from 'mysql'
+import { PrismaClient } from '@prisma/client'
+import fs from 'fs'
+import { parse } from 'csv-parse'
 
 const prisma = new PrismaClient()
-dotenv.config()
 
-const connection = mysql.createConnection({
-    user: 'root',
-    password: '',
-    database: 'store',
-    socketPath: '',
-})
-connection.connect()
+function sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
 
-// USERS
-connection.query(
-    'SELECT * FROM users',
-    async (
-        error: MysqlError,
-        res: {
-            username: string
-            password: string
-            type: UsersType
-        }[],
-        fields: FieldInfo[]
-    ) => {
-        if (error) throw error
-        await prisma.users.createMany({
-            data: res.map((r) => ({ ...r, name: r.username })),
+const f = async () => {
+    // USERS
+    fs.createReadStream(`${__dirname}/users.csv`)
+        .pipe(parse({ delimiter: ',', columns: true }))
+        .on('data', async (row) => {
+            try {
+                await prisma.users.create({
+                    data: row,
+                })
+                console.log(`User ${row['name']} created`)
+            } catch (e) {
+                console.log(e)
+                console.log(row)
+                process.exit(1)
+            }
         })
-    }
-)
-
-// CUSTOMER
-connection.query(
-    'SELECT * FROM customer',
-    async (
-        error: MysqlError,
-        res: {
-            code: string
-            name: string
-            address1: string
-            address2: string
-            city: string
-            state: string
-            GST_no: string
-            PAN_no: string
-        }[],
-        fields: FieldInfo[]
-    ) => {
-        if (error) throw error
-        await prisma.customer.createMany({
-            data: res.map((r) => ({
-                id: r.code,
-                name: r.name,
-                address1: r.address1,
-                address2: r.address2,
-                city: r.city,
-                state: r.state,
-                gst: r.GST_no,
-            })),
+        .on('end', async () => {
+            // SUPPLIER
+            await sleep(1000)
+            fs.createReadStream(`${__dirname}/supplier.csv`)
+                .pipe(parse({ delimiter: ',', columns: true }))
+                .on('data', async (row) => {
+                    try {
+                        await prisma.supplier.create({
+                            data: row,
+                        })
+                        console.log(`Supplier ${row['id']} created`)
+                    } catch (e) {
+                        console.log(e)
+                        console.log(row)
+                        process.exit(1)
+                    }
+                })
+                .on('end', async () => {
+                    // RAW MATERIAL
+                    await sleep(1000)
+                    fs.createReadStream(`${__dirname}/rm.csv`)
+                        .pipe(parse({ delimiter: ',', columns: true }))
+                        .on('data', async (row) => {
+                            try {
+                                await prisma.rm.create({
+                                    data: {
+                                        ...row,
+                                        price: parseFloat(row.price),
+                                        storeStock: parseFloat(row.storeStock),
+                                        lineStock: parseFloat(row.lineStock),
+                                    },
+                                })
+                                console.log(`RM ${row['id']} created`)
+                            } catch (e) {
+                                console.log(e)
+                                console.log(row)
+                                process.exit(1)
+                            }
+                        })
+                        .on('end', async () => {
+                            // CUSTOMER
+                            await sleep(1000)
+                            fs.createReadStream(`${__dirname}/customer.csv`)
+                                .pipe(parse({ delimiter: ',', columns: true }))
+                                .on('data', async (row) => {
+                                    try {
+                                        await prisma.customer.create({
+                                            data: row,
+                                        })
+                                        console.log(
+                                            `Customer ${row['id']} created`
+                                        )
+                                    } catch (e) {
+                                        console.log(e)
+                                        console.log(row)
+                                        process.exit(1)
+                                    }
+                                })
+                                .on('end', async () => {
+                                    // FINISHED GOODS
+                                    await sleep(1000)
+                                    fs.createReadStream(`${__dirname}/fg.csv`)
+                                        .pipe(
+                                            parse({
+                                                delimiter: ',',
+                                                columns: true,
+                                            })
+                                        )
+                                        .on('data', async (row) => {
+                                            try {
+                                                await prisma.fg.create({
+                                                    data: {
+                                                        ...row,
+                                                        price: parseFloat(
+                                                            row.price
+                                                        ),
+                                                        storeStock: parseFloat(
+                                                            row.storeStock
+                                                        ),
+                                                        overheads: parseFloat(
+                                                            row.overheads
+                                                        ),
+                                                        manPower: parseFloat(
+                                                            row.manPower
+                                                        ),
+                                                    },
+                                                })
+                                                console.log(
+                                                    `FG ${row['id']} created`
+                                                )
+                                            } catch (e) {
+                                                console.log(e)
+                                                console.log(row)
+                                                process.exit(1)
+                                            }
+                                        })
+                                        .on('end', async () => {
+                                            // BOM
+                                            await sleep(1000)
+                                            fs.createReadStream(
+                                                `${__dirname}/bom.csv`
+                                            )
+                                                .pipe(
+                                                    parse({
+                                                        delimiter: ',',
+                                                        columns: true,
+                                                    })
+                                                )
+                                                .on('data', async (row) => {
+                                                    try {
+                                                        await prisma.bom.create(
+                                                            {
+                                                                data: {
+                                                                    ...row,
+                                                                    quantity:
+                                                                        parseFloat(
+                                                                            row.quantity
+                                                                        ),
+                                                                },
+                                                            }
+                                                        )
+                                                    } catch (e) {
+                                                        console.log(e)
+                                                        console.log(row)
+                                                        process.exit(1)
+                                                    }
+                                                })
+                                        })
+                                })
+                        })
+                })
         })
-    }
-)
+}
 
-// SUPPLIER
-connection.query(
-    'SELECT * FROM supplier',
-    async (
-        error: MysqlError,
-        res: {
-            code: string
-            name: string
-            address1: string
-            address2: string
-            city: string
-            state: string
-            GST_no: string
-            PAN_no: string
-        }[],
-        fields: FieldInfo[]
-    ) => {
-        if (error) throw error
-        await prisma.supplier.createMany({
-            data: res.map((r) => ({
-                id: r.code,
-                name: r.name,
-                address1: r.address1,
-                address2: r.address2,
-                city: r.city,
-                state: r.state,
-                gst: r.GST_no,
-            })),
-        })
-    }
-)
-
-// RAW MATERIAL
-connection.query(
-    'SELECT * FROM raw_material',
-    async (
-        error: MysqlError,
-        res: {
-            code: string
-            DTPL_code: string
-            name: string
-            category: RmCategory | 'Misc.' | 'Conector'
-            stock: number
-            line_stock: number
-            unit: string
-            supplier_code: string
-            price: number
-            monthly_requirement: number
-        }[],
-        fields: FieldInfo[]
-    ) => {
-        if (error) throw error
-        await prisma.rm.createMany({
-            data: res.map((r) => ({
-                id: r.code,
-                description: r.name,
-                dtplCode: r.DTPL_code,
-                supplierId: r.supplier_code,
-                category:
-                    r.category === 'Misc.'
-                        ? RmCategory.Misc
-                        : r.category === 'Conector'
-                        ? RmCategory.Connector
-                        : r.category,
-                unit: r.unit,
-                price: r.price,
-                storeStock: r.stock,
-                lineStock: r.line_stock,
-            })),
-        })
-    }
-)
-
-// FINISHED GOODS
-connection.query(
-    'SELECT * FROM finished_goods',
-    async (
-        error: MysqlError,
-        res: {
-            code: string
-            name: string
-            customer: string
-            category: FgCategory
-            stock: number
-            quantity: number
-            price: number
-            overheads: number
-            man_power: number
-        }[],
-        fields: FieldInfo[]
-    ) => {
-        if (error) throw error
-        await prisma.fg.createMany({
-            data: res.map((r) => ({
-                id: r.code,
-                customerId: r.customer,
-                description: r.name,
-                storeStock: r.stock,
-                category: r.category,
-                price: r.price,
-                manPower: r.man_power,
-                overheads: r.overheads,
-            })),
-        })
-    }
-)
-
-// BOM
-connection.query(
-    'SELECT * FROM finished_goods_detail',
-    async (
-        error: MysqlError,
-        res: {
-            code: string
-            raw_material_code: string
-            quantity: number
-        }[],
-        fields: FieldInfo[]
-    ) => {
-        if (error) throw error
-        await prisma.bom.createMany({
-            data: res.map((r) => ({
-                fgId: r.code,
-                rmId: r.raw_material_code,
-                quantity: r.quantity,
-            })),
-        })
-    }
-)
-
-connection.end()
+f()
