f76ca02e46f4de41ec70fd6763c56b8fef5d7e90
Pranshu Maheshwari <maheshwari.pranshu511@gmail.com>
2024-04-05 18:51:57 +0530

    Added print option to Table

--- ui/src/components/Table.tsx
+++ ui/src/components/Table.tsx
@@ -1,8 +1,8 @@
 import { AgGridReact, AgGridReactProps } from 'ag-grid-react'
 import { Box, Skeleton } from '@mantine/core'
-import React, { useMemo } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 
-import { ColDef } from 'ag-grid-community'
+import { ColDef, GetContextMenuItemsParams, MenuItemDef } from 'ag-grid-community'
 
 export interface TableInterface<T> extends AgGridReactProps<T> {
     fileName?: string
@@ -17,6 +17,8 @@ function Table<Type>({
     domLayout = 'normal',
     ...otherProps
 }: TableInterface<Type>) {
+    const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), [])
+    const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), [])
     const cDefaultColDef = useMemo(
         () => ({
             sortable: true,
@@ -49,33 +51,60 @@ function Table<Type>({
         []
     )
 
+    const getContextMenuItems = useCallback((params: GetContextMenuItemsParams): (string | MenuItemDef)[] => [
+        "copy",
+        "export",
+        {
+            name: "Print",
+            action: () => {
+                const eGridDiv = document.querySelector<HTMLElement>("#myGrid")! as any
+                eGridDiv.style.width = ""
+                eGridDiv.style.height = ""
+                params.api.setGridOption("domLayout", "print")
+                setTimeout(() => {
+                    window.print() 
+                    const eGridDiv = document.querySelector<HTMLElement>("#myGrid")! as any
+                    eGridDiv.style.width = "100%"
+                    eGridDiv.style.height = "100%"
+                    params.api.setGridOption("domLayout", "autoHeight")
+                  }, 2000)
+            },
+          },
+    ], [window])
+
     return (
-        <Box w="100%" h="100%" className="ag-theme-alpine">
+        <Box style={containerStyle}>
             {rowData ? (
-                <AgGridReact<Type>
-                    animateRows
-                    columnDefs={columnDefs ? [
-                        {
-                            headerName: "#", 
-                            valueGetter: "node.rowIndex + 1",
-                            pinned: "left"
-                        }, ...columnDefs] : null}
-                    rowData={rowData}
-                    defaultColDef={cDefaultColDef}
-                    columnTypes={columnTypes}
-                    domLayout={domLayout}
-                    defaultExcelExportParams={{
-                        fileName: fileName ? fileName + '.xlsx' : 'export.xlsx',
-                        columnKeys: columnDefs
-                            ?.filter(
-                                (column: ColDef<Type>) => column.field !== '#'
-                            )
-                            .map((column: ColDef<Type>) =>
-                                column.field ? column.field : ''
-                            ),
-                    }}
-                    {...otherProps}
-                />
+                <div
+                id="myGrid"
+                style={gridStyle}
+                className="ag-theme-alpine">
+                    <AgGridReact<Type>
+                        animateRows
+                        columnDefs={columnDefs ? [
+                            {
+                                headerName: "#", 
+                                valueGetter: "node.rowIndex + 1",
+                                pinned: "left",
+                            }, ...columnDefs] : null}
+                        rowData={rowData}
+                        defaultColDef={cDefaultColDef}
+                        columnTypes={columnTypes}
+                        domLayout={domLayout}
+                        getContextMenuItems={getContextMenuItems}
+                        defaultExcelExportParams={{
+                            fileName: fileName ? fileName + '.xlsx' : 'export.xlsx',
+                            columnKeys: columnDefs
+                                ?.filter(
+                                    (column: ColDef<Type>) => column.field !== '#'
+                                )
+                                .map((column: ColDef<Type>) =>
+                                    column.field ? column.field : ''
+                                ),
+                        }}
+                        {...otherProps}
+                    />
+                </div>
             ) : (
                 <>
                     <Skeleton width="100%" height="100%" />
